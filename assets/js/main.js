486ead962fc17b421b4d976b1fd6c86f4331d989
Anatoli Kalysch <anatoli.kalysch@fau.de>
2019-07-03 13:26:59 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,97 @@
+/*
+	Visualize by Pixelarity
+	pixelarity.com | hello@pixelarity.com
+	License: pixelarity.com/license
+*/
+
+(function($) {
+
+	var	$window	= $(window),
+		$header = $('#header'),
+		$banner	= $('#banner'),
+		$body	= $('body');
+
+	// Breakpoints.
+		breakpoints({
+			default:   ['1681px',   null       ],
+			xlarge:    ['1281px',   '1680px'   ],
+			large:     ['981px',    '1280px'   ],
+			medium:    ['737px',    '980px'    ],
+			small:     ['481px',    '736px'    ],
+			xsmall:    ['361px',    '480px'    ],
+			xxsmall:   [null,       '360px'    ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Scrolly.
+		$('.scrolly').scrolly({
+			offset: function() { return $header.height() - 5; }
+		});
+
+	// Tweaks/fixes.
+
+		// Polyfill: Object fit.
+			if (!browser.canUse('object-fit')) {
+
+				$('.image[data-position]').each(function() {
+
+					var $this = $(this),
+						$img = $this.children('img');
+
+					// Apply img as background.
+						$this
+							.css('background-image', 'url("' + $img.attr('src') + '")')
+							.css('background-position', $this.data('position'))
+							.css('background-size', 'cover')
+							.css('background-repeat', 'no-repeat');
+
+					// Hide img.
+						$img
+							.css('opacity', '0');
+
+				});
+
+			}
+
+		// IE-specific fixes.
+			if (browser.name == 'ie'
+			||	browser.name == 'edge')
+				$body.addClass('is-ie');
+
+	// Header.
+		if ($banner.length > 0
+		&& $header.hasClass('alt')) {
+
+			$window.on('resize', function() { $window.trigger('scroll'); });
+
+			$banner.scrollex({
+				bottom:		$header.outerHeight(),
+				terminate:	function() { $header.removeClass('alt'); },
+				enter:		function() { $header.addClass('alt'); },
+				leave:		function() { $header.removeClass('alt'); $header.addClass('reveal'); }
+			});
+
+		}
+
+	// Menu.
+		$('#menu')
+			.append('<a href="#menu" class="close"></a>')
+			.appendTo($body)
+			.panel({
+				target: $body,
+				visibleClass: 'is-menu-visible',
+				delay: 500,
+				hideOnClick: true,
+				hideOnSwipe: true,
+				resetScroll: true,
+				resetForms: true,
+				side: 'right'
+			});
+
+})(jQuery);
